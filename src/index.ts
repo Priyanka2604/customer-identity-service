258c81803e6485aaefb1baa14e00b5a574a6ad28
Priyanka Das <priyankadas@Priyankas-MacBook-Air.local>
2025-07-04 19:02:09 +0530

    add identify route

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
 // App entry point
 import express from 'express';
 import dotenv from 'dotenv';
+import contactRoutes from './routes/contact.route';
+
 //import contactRoutes from './routes/contact.route';
 
 dotenv.config();
 const app = express();
-app.use(express.json());
-
-//app.use('/identify', contactRoutes);
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Bitespeed Identity Resolver API is running.');
 });
 
+app.use('/', contactRoutes);
+
 app.use((req, res) => {
   res.status(404).send("Route not found");
 });
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
